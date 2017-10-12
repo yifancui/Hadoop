99c2d0c14654ee8c6ee18cb4a6bf1ee04eda94fd
yifancui <xcui2@wpi.edu>
2017-10-12 13:25:19 -0400

    Add files via upload

---
+++ generatedata.java
@@ -0,0 +1,46 @@
+/* Backup code for data generation.
+   Please attempt to use code in data_creation folder first. */
+
+package hw2;
+
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.FilterWriter;
+import java.io.IOException;
+import java.util.Random;
+
+
+public class generatedata {
+	public static Random numGen =new Random();
+	
+	
+
+	public static int RandNum(){
+		
+	    int rand = Math.abs((1)+numGen.nextInt(10000));
+
+	    return rand;
+	}
+
+	public static void main(String[]Args) throws IOException{
+		FileWriter f0 = new FileWriter("/home/yifan/data.txt");
+		numGen.setSeed(1);
+			
+		for(int i=1;i<10000000;i++){
+			String a=Integer.toString(RandNum());
+			String b=Integer.toString(RandNum());
+			String output = a+","+b+"\n";   //ans from other logic
+		    
+		    f0.write(output);
+				
+			
+			//System.out.println(a+","+b);
+		}
+		f0.close();
+	   
+	}
+		
+	
+
+}
